ed4e558cf2e496d852e1f3406a7887d3d29f4079
tom <294364706@qq.com>
2020-12-22 17:38:49 +0800

    create HelloWorld

---
+++ 01-javase/src/com/cy/java/basic/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.cy.java.basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("hello");
+        System.out.println(args.length);
+    }
+}
